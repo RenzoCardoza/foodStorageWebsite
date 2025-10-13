3eebe70176ecffdb4463f0e8b58d2cbba90ba68d
Renzo Cardoza <car22001@byui.edu>
2025-10-13 19:37:44 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# foodStorageWebsite
-Repository for project for BYU Idaho course
+# Food Storage Website Project
+Repository for project for BYU Idaho course Sustaining Human Life
+
+## Details
+This is a project made into Node.js to create a web app that is accesible for many people.
+
+Later details will be provided later on.
---
+++ server.js
@@ -0,0 +1,44 @@
+/* ***********************
+ * Require Statements
+ *************************/
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static.js");
+const utilities = require("./utilities/index.js");
+const baseController = require("./controllers/baseControllers");
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout") //not at views root
+
+
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(static)
+//Index route 
+app.get("/", utilities.handleErrors(baseController.buildHome))
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+const port = process.env.PORT
+const host = process.env.HOST
+
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
+})
